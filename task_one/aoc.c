63dba1d42d6eedd02c9cfc79aafeccea95ec5ba5
Marlon Monzon <martianworm17@gmail.com>
2019-06-24 23:36:34 +0800

    [WIP] First task

--- task_one/aoc.c
+++ task_one/aoc.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<ncurses.h>
+#include<string.h>
 
 float start_point;
 float end_point;
@@ -8,5 +9,15 @@ float total_area;
 int numtraps;
 
 void main () {
-    
+
+    void input(void);
+    float find_area(float a, float b, int n);
+
+    printf("AREA UNDER THE CURVE");
+
+    input();
+    total_area = find_area(start_point, end_point, numtraps);
+
+    printf("The total area is %f", total_area);
+
 }
