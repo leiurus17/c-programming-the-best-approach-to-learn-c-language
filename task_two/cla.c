cf9fa32a0b8a5fe523f61b8ae0164eb799e387d6
Marlon Monzon <martianworm17@gmail.com>
2019-06-27 22:24:23 +0800

    Open the file and write the arguments

--- task_two/cla.c
+++ task_two/cla.c
@@ -2,10 +2,16 @@
 
 void main(int argc, char* argv[]) {
 
-FILE *fp;
-int i;
-char word[15];
+    FILE *fp;
+    int i;
+    char word[15];
 
-fp = fopen(argv[1], "w"); // write on the file
+    fp = fopen(argv[1], "w"); // write on the file
 
+    printf("Number of command line arguments = %d", argc);
+
+    for (i = 2; i < argc; i++) {
+        
+        fprintf(fp, "%s", argv[i]);
+    }
 }
