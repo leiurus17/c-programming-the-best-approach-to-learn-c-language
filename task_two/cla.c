4096720155cc30593c41f9fa914df379664a110f
Marlon Monzon <martianworm17@gmail.com>
2019-06-28 22:33:26 +0800

    Write on the file and then read the contents of the file

--- task_two/cla.c
+++ task_two/cla.c
@@ -15,5 +15,21 @@ void main(int argc, char* argv[]) {
         fprintf(fp, "%s", argv[i]);
     }
 
-    fclose(fp);
+    fclose(fp); // close the file
+
+    printf("\n\n");
+
+    printf("Contents of the %s file\n\n", argv[1]);
+    fp = fopen(argv[1], "r");
+
+    for (i = 2; i < argc; i++) {
+        fscanf(fp, "%s", word);
+        printf("%s", word);
+    }
+
+    for (i = 0; i < argc; i++) {
+        printf("%s", argv[i]);
+    }
+
+    fclose(fp); //do not forget to close the file
 }
