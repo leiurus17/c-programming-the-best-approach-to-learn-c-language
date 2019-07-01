3c67aef3e9290d62f951e400070e79ff3bf7b85a
Marlon Monzon <martianworm17@gmail.com>
2019-07-01 22:07:23 +0800

    Add comments

--- pointer.c
+++ pointer.c
@@ -7,19 +7,21 @@ int main() {
 
     int *p; // pointer
 
-    printf("Zero: %d\n", *p);
+    printf("Zero: %d\n", *p); // What is this value?
 
     printf("First: %d\n", p); // What is this value?
 
-    p = & item;
+    p = & item; // p now holds the address of the memory containing the value of item variable
 
-    printf("Second: %d\n", p);
+    printf("Second: %d\n", p); // Is this the address?
 
-    printf("Third: %d\n", n);
+    printf("Third: %d\n", n); // What is this value? The size of int?
 
     n = *p;
 
-    printf("Fourth: %d\n", n);
+    printf("Fourth: %d\n", n); // The value of item is now copied on n
+
+    printf("Fifth: %d\n", item);
 
     return(0);
 }
