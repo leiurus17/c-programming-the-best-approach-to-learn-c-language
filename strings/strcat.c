d5e73afc5cc3ba72e220a67b167a98b506fa3dde
Marlon Monzon <martianworm17@gmail.com>
2019-07-09 00:19:53 +0800

    strcat()

---
+++ strings/strcat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+    char first_name[50] = "marlon";
+    char last_name[50] = "monzon";
+
+    printf(strcat(first_name, last_name));
+
+    return (0);
+}
