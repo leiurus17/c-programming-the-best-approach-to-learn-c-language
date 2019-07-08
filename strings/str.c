fa03495595d7d6365338e5e25d00f4d121927e14
Marlon Monzon <martianworm17@gmail.com>
2019-07-09 00:31:00 +0800

    strlen()

--- strings/str.c
+++ strings/str.c
@@ -8,12 +8,19 @@ int main () {
     printf("%s\n", string_one);
     printf("%s\n", string_two);
 
+    printf("%d\n", strlen(string_one));
+    printf("%d\n", strlen(string_two));
+
+
     strcpy(string_one, string_two);
 
     printf("---\n");
 
     printf("%s\n", string_one);
     printf("%s\n", string_two);
+
+    printf("%d\n", strlen(string_one));
+    printf("%d\n", strlen(string_two));
 
     return(0);
 }
