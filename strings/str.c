9ab417e41ae8fc3f0a2e00b2ccd1f33ac606c475
Marlon Monzon <martianworm17@gmail.com>
2019-07-09 00:29:14 +0800

    strcpy()

---
+++ strings/str.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main () {
+    char string_one[50] = "marlon";
+    char string_two[50] = "pogi";
+
+    printf("%s\n", string_one);
+    printf("%s\n", string_two);
+
+    strcpy(string_one, string_two);
+
+    printf("---\n");
+
+    printf("%s\n", string_one);
+    printf("%s\n", string_two);
+
+    return(0);
+}
