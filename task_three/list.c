a793b7b1dfabff5f0126ee6ed5f9a778b2103b04
Marlon Monzon <martianworm17@gmail.com>
2019-07-08 11:00:29 +0800

    Task Three : Development savepoint

--- task_three/list.c
+++ task_three/list.c
@@ -11,9 +11,41 @@ struct linked_list {
 typedef struct linked_list node;
 
 int main () {
+    int n;
+    node *head;
 
-    
+    void create(node *p);
+    node insert(node *p, int n);
+    void print(node *p);
 
+    head = (node *) malloc(sizeof(node));
+
+    create(head);
+
+    printf("\n");
+    printf("original list : ");
+    printf(head);
+    printf("\n\n");
+    print("input the number to be inserted");
+
+    scanf("%d", &n);
+
+    // head = insert(*head, n); // TODO
+
+    printf("\n");
+    printf("new list after insertion : ");
+    printf(head);
+
+    getch();
 
     return(0);
+}
+
+void create(node *p) {
+}
+
+node insert(node *p, int n) {
+}
+
+void print(node *p) {
 }
