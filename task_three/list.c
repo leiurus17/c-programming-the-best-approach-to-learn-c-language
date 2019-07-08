4bf99c454dd55418f3ff806625feea18bd60ba7f
Marlon Monzon <martianworm17@gmail.com>
2019-07-08 10:36:45 +0800

    Task Three : Development savepoint

---
+++ task_three/list.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ncurses.h>
+
+#define NULL 0
+
+struct linked_list {
+    int number;
+    struct linked_list *next;   
+};
+
+typedef struct linked_list node;
+
+int main () {
+
+    
+
+
+    return(0);
+}
